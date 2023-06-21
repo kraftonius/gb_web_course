b6421e94cfccc7b3efc9b2e197828f1f1da24042
kraftonius <avd@yandex.ru>
2023-06-21 21:38:59 +0300

    add Homework4

---
+++ seminar4Homework/sql.sql
@@ -0,0 +1,14 @@
+-- create
+CREATE TABLE STUDENT (
+id INTEGER PRIMARY KEY,
+name TEXT NOT NULL,
+age TEXT NOT NULL,
+city TEXT NOT NULL
+);
+
+-- insert
+INSERT INTO STUDENT VALUES (001, 'Петр', '20', 'Санкт-Петербург');
+INSERT INTO STUDENT VALUES (002, 'Владимир', '19', 'Белгород');
+INSERT INTO STUDENT VALUES (003, 'Варвара', '23', 'Владивосток');
+INSERT INTO STUDENT VALUES (004, 'Антон', '31', 'Челябинск');
+INSERT INTO STUDENT VALUES (005, 'Афанасий', '42', 'Рязань');
